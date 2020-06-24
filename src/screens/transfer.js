d68b1b28a71a9ab76590d230a9c2f34b8080b04e
Peter Atef <p.atef@hotmail.com>
2020-06-24 12:01:55 +0200

    fixing transfer keyboard problem

--- src/screens/transfer.js
+++ src/screens/transfer.js
@@ -14,6 +14,9 @@ import {
   Image,
   ScrollView,
   Alert,
+  TouchableWithoutFeedback,
+  KeyboardAvoidingView,
+  Keyboard,
 } from "react-native";
 import Toast from "react-native-tiny-toast";
 
@@ -22,12 +25,7 @@ import { bindActionCreators, Dispatch } from "redux";
 import { State } from "../state/state";
 import { transfer } from "../state/transfer/action-creator";
 
-
-import {
-  ToastAndroid,
-  Platform,
-  AlertIOS,
-} from 'react-native';
+import { ToastAndroid, Platform, AlertIOS } from "react-native";
 
 import {
   widthPercentageToDP as wp,
@@ -45,7 +43,7 @@ class TransferContainer extends Component {
       amount: "",
       senderCodeNo: "",
       pin: "",
-      mobile:""
+      mobile: "",
     };
   }
 
@@ -86,12 +84,16 @@ class TransferContainer extends Component {
   }
   onHandleConfirmation() {
     if (
-    (this.state.receiverCodeNo.length <= 0 && this.state.mobile.length<=0) ||
+      (this.state.receiverCodeNo.length <= 0 &&
+        this.state.mobile.length <= 0) ||
       this.state.pin.length <= 0 ||
       this.state.amount.length <= 0
     ) {
       let alertMessage;
-      if (this.state.receiverCodeNo.length <= 0 && this.state.mobile.length<=0) {
+      if (
+        this.state.receiverCodeNo.length <= 0 &&
+        this.state.mobile.length <= 0
+      ) {
         alertMessage = "You need to insert the reciever ";
       } else if (this.state.amount.length <= 0) {
         alertMessage = "You need insert the amount";
@@ -126,8 +128,8 @@ class TransferContainer extends Component {
       // setTimeout(function () {
       //   Toast.hide(toast);
       //   },9000);
-      // 
-      if (Platform.OS === 'android') {
+      //
+      if (Platform.OS === "android") {
         debugger;
         ToastAndroid.show(result["Message"], ToastAndroid.SHORT);
         this.clearState();
@@ -143,104 +145,118 @@ class TransferContainer extends Component {
       this.state.receiverCodeNo.length > 0 && this.state.pin.length > 0;
 
     return (
-      // trasnfer Component
-      <View style={styles.container}>
-        <View style={styles.centerTitle}>
-          <Text style={{ fontWeight: "bold", color: "#D0C21D", fontSize: 16 }}>
-            Transfer
-          </Text>
-        </View>
-        <View style={styles.centerLogo}>
-          <Image source={logo} style={{ width: 150 }} />
-        </View>
-        <View style={styles.eachRowAccount}>
-          <Text style={styles.inputTitleText}>From Account</Text>
-          <Text style={{ width: "50%" }}> #{this.props.membershipId}</Text>
-        </View>
-        <View style={styles.eachRowAccount}>
-          <Text style={styles.inputTitleText}>Balance</Text>
-          <Text style={{ width: "50%" }}> {this.props.balance}EGP</Text>
-        </View>
+      <ScrollView style={{ backgroundColor: "white" }}>
+        <KeyboardAvoidingView behavior="position" keyboardVerticalOffset={30}>
+          <TouchableWithoutFeedback
+            onPress={() => {
+              Keyboard.dismiss();
+            }}
+          >
+            <View style={styles.container}>
+              <View style={styles.centerTitle}>
+                <Text
+                  style={{ fontWeight: "bold", color: "#D0C21D", fontSize: 16 }}
+                >
+                  Transfer
+                </Text>
+              </View>
+              <View style={styles.centerLogo}>
+                <Image source={logo} style={{ width: 150 }} />
+              </View>
+              <View style={styles.eachRowAccount}>
+                <Text style={styles.inputTitleText}>From Account</Text>
+                <Text style={{ width: "50%" }}>
+                  {" "}
+                  #{this.props.membershipId}
+                </Text>
+              </View>
+              <View style={styles.eachRowAccount}>
+                <Text style={styles.inputTitleText}>Balance</Text>
+                <Text style={{ width: "50%" }}> {this.props.balance}EGP</Text>
+              </View>
 
-        <View style={styles.eachRow}>
-          <Text style={styles.inputTitleText}>Search By</Text>
-          <View style={{ width: "50%", marginTop: "3%" }}>
-            <RadioForm
-              formHorizontal={true}
-              radio_props={[
-                { label: "Mob", value: "mob" },
-                { label: "Id", value: "id" },
-              ]}
-              initial={0}
-              buttonColor={"#D0C21D"}
-              selectedButtonColor={"#D0C21D"}
-              radioStyle={{ paddingRight: 25 }}
-              onPress={(value) => {
-                this.onChooseInput(value);
-              }}
-            />
-          </View>
-        </View>
+              <View style={styles.eachRow}>
+                <Text style={styles.inputTitleText}>Search By</Text>
+                <View style={{ width: "50%", marginTop: "3%" }}>
+                  <RadioForm
+                    formHorizontal={true}
+                    radio_props={[
+                      { label: "Mob", value: "mob" },
+                      { label: "Id", value: "id" },
+                    ]}
+                    initial={0}
+                    buttonColor={"#D0C21D"}
+                    selectedButtonColor={"#D0C21D"}
+                    radioStyle={{ paddingRight: 25 }}
+                    onPress={(value) => {
+                      this.onChooseInput(value);
+                    }}
+                  />
+                </View>
+              </View>
 
-        {this.state.type === "mob" ? (
-          <View style={styles.eachRow}>
-            <Text style={styles.inputTitle}>Mobile</Text>
-            <TextInput
-              value={this.state.mobile}
-              onChangeText={(mobile) => this.setState({ mobile })}
-              placeholder={"Member Mob. number"}
-              placeholderTextColor="#202945"
-              keyboardType="numeric"
-              style={styles.input}
-            />
-          </View>
-        ) : (
-          <View style={styles.eachRow}>
-            <Text style={styles.inputTitle}>To Account</Text>
-            <TextInput
-              value={this.state.receiverCodeNo}
-              onChangeText={(receiverCodeNo) =>
-                this.setState({ receiverCodeNo })
-              }
-              placeholder={"To Account"}
-              placeholderTextColor="#202945"
-              keyboardType={"numeric"}
-              style={styles.input}
-            />
-          </View>
-        )}
+              {this.state.type === "mob" ? (
+                <View style={styles.eachRow}>
+                  <Text style={styles.inputTitle}>Mobile</Text>
+                  <TextInput
+                    value={this.state.mobile}
+                    onChangeText={(mobile) => this.setState({ mobile })}
+                    placeholder={"Member Mob. number"}
+                    placeholderTextColor="#202945"
+                    keyboardType="numeric"
+                    style={styles.input}
+                  />
+                </View>
+              ) : (
+                <View style={styles.eachRow}>
+                  <Text style={styles.inputTitle}>To Account</Text>
+                  <TextInput
+                    value={this.state.receiverCodeNo}
+                    onChangeText={(receiverCodeNo) =>
+                      this.setState({ receiverCodeNo })
+                    }
+                    placeholder={"To Account"}
+                    placeholderTextColor="#202945"
+                    keyboardType={"numeric"}
+                    style={styles.input}
+                  />
+                </View>
+              )}
 
-        <View style={styles.eachRow}>
-          <Text style={styles.inputTitle}>Amount</Text>
-          <TextInput
-            value={this.state.amount}
-            onChangeText={(e) => this.onTextChanged(e)}
-            placeholder={"Enter Amount"}
-            placeholderTextColor="#202945"
-            keyboardType={"numeric"}
-            style={styles.input}
-          />
-        </View>
-        <View style={styles.eachRow}>
-          <Text style={styles.inputTitle}>Pin</Text>
-          <TextInput
-            value={this.state.pin}
-            onChangeText={(pin) => this.setState({ pin })}
-            placeholder={"Enter Pin"}
-            placeholderTextColor="#202945"
-            keyboardType="numeric"
-            style={styles.input}
-          />
-        </View>
-        <Button
-          // disabled={!isEnabled}
-          style={styles.buttonStyle}
-          // onPress={this.makeTransfer.bind(this)}
-          onPress={() => this.onHandleConfirmation()}
-        >
-          <Text style={{ color: "#202945" }}>Confirm</Text>
-        </Button>
-      </View>
+              <View style={styles.eachRow}>
+                <Text style={styles.inputTitle}>Amount</Text>
+                <TextInput
+                  value={this.state.amount}
+                  onChangeText={(e) => this.onTextChanged(e)}
+                  placeholder={"Enter Amount"}
+                  placeholderTextColor="#202945"
+                  keyboardType={"numeric"}
+                  style={styles.input}
+                />
+              </View>
+              <View style={styles.eachRow}>
+                <Text style={styles.inputTitle}>Pin</Text>
+                <TextInput
+                  value={this.state.pin}
+                  onChangeText={(pin) => this.setState({ pin })}
+                  placeholder={"Enter Pin"}
+                  placeholderTextColor="#202945"
+                  keyboardType="numeric"
+                  style={styles.input}
+                />
+              </View>
+              <Button
+                // disabled={!isEnabled}
+                style={styles.buttonStyle}
+                // onPress={this.makeTransfer.bind(this)}
+                onPress={() => this.onHandleConfirmation()}
+              >
+                <Text style={{ color: "#202945" }}>Confirm</Text>
+              </Button>
+            </View>
+          </TouchableWithoutFeedback>
+        </KeyboardAvoidingView>
+      </ScrollView>
     );
   }
 }
